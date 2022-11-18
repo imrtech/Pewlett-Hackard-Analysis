8ce4d51716d69e39468c7c5d38fb5c16312f1979
Irma Rivera <irivera@phinational.org>
2022-11-18 15:42:19 -0500

    updated schema.sql file

--- schema.sql
+++ schema.sql
@@ -45,14 +45,21 @@ CREATE TABLE dept_emp (
   PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE Titles (
-  emp_no INT NOT NULL,
-  title VARCHAR(40) NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE,
+    FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, title, from_date)
 );
+
+DROP TABLE titles CASCADE;
 
 
 SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles; 
